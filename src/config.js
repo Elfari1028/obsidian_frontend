5bfb8117410cf3decc39ff24c3016411e5353e91
alpharabdugeni <alpharpledez@outlook.com>
2020-08-19 19:52:21 +0800

    base url fix

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 export default class Config {
-    static baseUrl = 'http://39.97.104.62/'
+    static baseUrl = 'http://39.97.104.62/api/'
     static infoUrl = this.baseUrl + 'account/my_status/'
-    static wsUrl = 'http://39.97.104.62/'
+    static wsUrl = 'http://39.97.104.62/api/'
     static axiosHeaders = {
         headers: {
             'Content-Type': 'application/json;charset=UTF-8'
         }
     }
-    static avatarUploadUrl = 'http://39.97.104.62/account/upload_avatar/'
+    static avatarUploadUrl = 'http://39.97.104.62/api/account/upload_avatar/'
 }
