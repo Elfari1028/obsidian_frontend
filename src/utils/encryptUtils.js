115de5292d68b2023ef6b8b4b09459e6d12e478f
BingoIO_OI233 <wyhchris@qq.com>
2020-08-19 10:40:15 +0800

    decryption

--- src/utils/encryptUtils.js
+++ src/utils/encryptUtils.js
@@ -2,11 +2,17 @@ import CryptoJS from "crypto-js";
 
 export function encryption(sourceForm, resultForm) {
     let key = generateKey()
-    for(let formKey in sourceForm)
-    {
+    for (let formKey in sourceForm) {
         resultForm[formKey] = encrypt(sourceForm[formKey], key)
     }
     resultForm.key = key
+}
+
+export function decryption(sourceForm, resultForm) {
+    let key = sourceForm.key
+    for (let formKey in sourceForm && formKey !== 'key') {
+        resultForm[formKey] = decrypt(sourceForm[formKey], key)
+    }
 }
 
 function generateKey() {
@@ -28,4 +34,15 @@ function encrypt(data, key) {
         mode: CryptoJS.mode.CBC,
         padding: CryptoJS.pad.ZeroPadding
     }).toString()
+}
+
+function decrypt(data, key) {
+    let iv = CryptoJS.MD5(key).toString().substring(0, 16);
+    key = CryptoJS.enc.Utf8.parse(key)
+    iv = CryptoJS.enc.Utf8.parse(iv)
+    return CryptoJS.AES.decrypt(data, key, {
+        iv: iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.ZeroPadding
+    })
 }
--- src/views/AvatarTest.vue
+++ src/views/AvatarTest.vue
@@ -1,44 +1,44 @@
 <template>
-    <el-form>
-        <div style="width: 100px;height: 100px">
-            <el-upload class="avatar_uploader"
-                       action="http://127.0.0.1:8000/account/upload_avatar/"
-                       :show-file-list="false"
-                       :on-success="handleAvatarSuccess"
-                       :before-upload="beforeAvatarUpload"
-                       :with-credentials="true"
-                       :name="'avatar'">
-                <el-avatar v-if="imageUrl!==''" shape="square" :size="100" :src="this.imageUrl"></el-avatar>
-                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-            </el-upload>
-        </div>
-
+    <el-form style="margin-top: 10px;width: 300px">
+        <el-form-item>
+            <el-input v-model="sourceForm.email" placeholder="val1" @keyup.enter.native="submit"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-input type="password" v-model="sourceForm.password" placeholder="val2"
+                      show-password @keyup.enter.native="submit"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="info" style="float: right" @click="submit">登录
+                <i class="el-icon-arrow-right"></i>
+            </el-button>
+        </el-form-item>
     </el-form>
 
 </template>
 
 <script>
+    import {decryption, encryption} from "@/utils/encryptUtils";
+
     export default {
         name: "AvatarTest",
         data() {
             return {
-                imageUrl: ''
+                sourceForm: {
+                    email: '',
+                    password: '',
+                },
+                resultForm: {},
+                testForm: {}
             }
         },
         methods: {
-            handleAvatarSuccess(res, file) {
-                this.imageUrl = URL.createObjectURL(file.raw)
-            },
-            beforeAvatarUpload(file) {
-                const formatValid = ((file.type === 'image/jpeg') || (file.type === 'image/png'))
-                const isLt2M = file.size / 1024 / 1024 < 2
-                if (!isLt2M) {
-                    this.$message.error('上传头像大小不能超过2MB')
-                }
-                if (!formatValid) {
-                    this.$message.error('上传头像只能为png或jpg格式')
-                }
-                return isLt2M && formatValid
+            submit() {
+                encryption(this.sourceForm, this.resultForm)
+                console.log(this.sourceForm)
+                console.log(this.resultForm)
+                decryption(this.resultForm, this.testForm)
+                console.log(this.resultForm)
+                console.log(this.sourceForm)
             }
         }
     }
